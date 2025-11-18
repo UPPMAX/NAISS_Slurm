a680cf1d494c35d9369c9e5f7925f9d5f7c12871
Joachim Hein <joachim.hein@math.lu.se>
2025-11-18 16:19:07 +0100

    Added sample script and program for MPI testing on Tetralith

---
+++ exercises/tetralith/MPI_examples/run_integration2D.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set account 
+#SBATCH -A <project ID> 
+
+# Set the time, 
+#SBATCH -t 00:10:00
+
+# ask for 10 core here, modify for your needs.
+# Aim to use multiples of 32 for larger jobs
+#SBATCH -n 20 # Asking for 10 core
+
+# name output and error file
+#SBATCH -o mpi_process_%j.out
+#SBATCH -e mpi_process_%j.err
+
+# write this script to stdout-file - useful for scripting errors
+cat $0
+
+# Run your mpi_executable
+#mpprun ./integration2D_f90 10000
+mpprun ./integration2D_c 10000
