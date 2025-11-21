6fe49f35659fc8de330c7ee02135773a586a234c
Joachim Hein <joachim.hein@math.lu.se>
2025-11-21 12:10:43 +0100

    Update for the Dardel MPI examples, demonstrating multinode running.

---
+++ exercises/dardel/MPI/run_mpi_hello_multinode.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set account 
+#SBATCH -A <project ID> 
+
+# Set the time, 
+#SBATCH -t 00:10:00
+
+# Using the Dardel's main partition
+#SBATCH -p main
+
+# ask for 256 cores located on 2 nodes, modify for your needs.
+
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=128
+
+# name output and error file
+#SBATCH -o mpi_process_%j.out
+#SBATCH -e mpi_process_%j.err
+
+# write this script to stdout-file - useful for scripting errors
+cat $0
+
+# Loading a suitable module. Here for Cray programming environment etc.
+module load PDC/24.11
+
+# Run your mpi_executable
+srun ./mpi_hello
