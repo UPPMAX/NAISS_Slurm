22daedf251e04e74b9c6a80a909328d5a63e5a02
Diana Iusan <diana.iusan@uppmax.uu.se>
2025-11-24 16:41:02 +0100

    update Pelle exercises

--- exercises/pelle/job-array/hello-world-array.sh
+++ exercises/pelle/job-array/hello-world-array.sh
@@ -5,6 +5,7 @@
 #SBATCH --time=00:05:00 # Asking for 5 minutes
 #SBATCH --array=1-10   # how many tasks in the array 
 #SBATCH -c 1 # Asking for 1 core    # one core per task 
+#SBATCH --hint=nomultithread
 # Setting the name of the output file 
 #SBATCH -o hello-world-%j-%a.out
 
